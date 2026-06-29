13da6b3565431df0ddb873780ff9d808221ecd8d
HannanehRaf <hannarafiee480@gmail.com>
2026-06-29 17:29:58 +0330

    Add Login and Signup

---
+++ frontend/app/home/page.tsx
@@ -0,0 +1,7 @@
+export default function HomePage() {
+      return (
+         <main className="min-h-screen flex items-center justify-center bg-black text-white">
+             <h1 className="text-3xl font-bold">Welcome to Shpotify</h1>
+         </main>
+      );
+}
